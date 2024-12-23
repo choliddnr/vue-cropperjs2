7d81b0dcb50303331eb4cdbf5336d780fe7013c0
choliddnr <cholid.dnr@gmail.com>
2024-12-23 16:55:02 +0700

    remove template ref and defineExpose

--- src/components/CropperCanvas.vue
+++ src/components/CropperCanvas.vue
@@ -1,42 +1,42 @@
 <script setup lang="ts">
 import { CropperCanvas } from "cropperjs";
-import { h, useTemplateRef } from "vue";
+import { h } from "vue";
 
 type CropperCanvasProps = {
   background?: boolean;
-  disabled?: boolean;
-  scaleStep?: number;
-  themeColor?: string;
+  // disabled?: boolean;
+  // scaleStep?: number;
+  // themeColor?: string;
 };
 
-const canvasRef = useTemplateRef<CropperCanvas>("canvasRef");
+// const canvasRef = useTemplateRef<CropperCanvas>("canvasRef");
 
 CropperCanvas.$define();
 
 const {
   background = false,
-  disabled = false,
-  scaleStep = 0.1,
-  themeColor = "#39f",
+  // disabled = false,
+  // scaleStep = 0.1,
+  // themeColor = "#39f",
 } = defineProps<CropperCanvasProps>();
 
 const el = h("cropper-canvas", {
   background,
-  disabled,
-  scaleStep,
-  themeColor,
+  // disabled,
+  // scaleStep,
+  // themeColor,
   style: "height: 500px",
 });
-const $setAction = canvasRef.value?.$setAction;
-const $toCanvas = canvasRef.value?.$toCanvas;
+// const $setAction = canvasRef.value?.$setAction;
+// const $toCanvas = canvasRef.value?.$toCanvas;
 
-defineExpose({
-  $setAction,
-  $toCanvas,
-});
+// defineExpose({
+//   $setAction,
+//   $toCanvas,
+// });
 </script>
 <template>
-  <component :is="el" ref="canvasRef">
+  <component :is="el">
     <slot />
   </component>
 </template>
